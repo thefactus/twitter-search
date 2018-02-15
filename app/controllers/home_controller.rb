908bab45b9d4f57c2f26947520503fd899f32700
Pablo Bello <thefactus@gmail.com>
2018-02-15 00:00:50 -0300

    Add twitter search feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
   def index
     @q = params[:q]
+    @tweets = search(@q)
+  end
+
+  private
+
+  def search(search_term)
+    return if @q&.empty?
+    TwitterClient
+      .search("#{search_term} -rt", result_type: 'recent')
+      .take(10)
   end
 end
