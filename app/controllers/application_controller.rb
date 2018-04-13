5127e6bcc19ea7544cd345cb4b936d52a5781db4
Pablo Bello <thefactus@gmail.com>
2018-04-13 19:47:45 -0300

    add generic redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  def generic_redirect(path, flash_hash = {})
+    flash_hash.each do |k, v|
+      flash[k] = v
+    end
+
+    redirect_to path
+  end
 end
