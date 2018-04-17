e27c66580d01ed154a4c180606c7580985bbc98d
Pablo Bello <thefactus@gmail.com>
2018-04-17 16:11:17 -0300

    Remove trailing whitespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
   def generic_redirect(path, flash_hash = {})
     flash_hash.each do |k, v|
       flash[k] = v
