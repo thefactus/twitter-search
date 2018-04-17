343284bbdff986448ecbd09072e94828981ec0dc
Pablo Bello <thefactus@gmail.com>
2018-04-17 18:58:37 -0300

    refactor double-quoted strings

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -7,9 +7,9 @@ if Rails.env == "production"
   end
 else
   TwitterClient = Twitter::REST::Client.new do |config|
-    config.consumer_key        = "YOUR_CONSUMER_KEY"
-    config.consumer_secret     = "YOUR_CONSUMER_SECRET"
-    config.access_token        = "YOUR_ACCESS_TOKEN"
-    config.access_token_secret = "YOUR_ACCESS_SECRET"
+    config.consumer_key        = 'YOUR_CONSUMER_KEY'
+    config.consumer_secret     = 'YOUR_CONSUMER_SECRET'
+    config.access_token        = 'YOUR_ACCESS_TOKEN'
+    config.access_token_secret = 'YOUR_ACCESS_SECRET'
   end
 end
